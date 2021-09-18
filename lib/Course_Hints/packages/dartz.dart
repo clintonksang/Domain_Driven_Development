92f40ec8a86e3b2972dad284beb3f4c2e31cbbf6
clintonksang <sangclint0@gmail.com>
2021-09-18 16:34:51 +0300

    Freezed Package

--- lib/Course_Hints/packages/dartz.dart
+++ lib/Course_Hints/packages/dartz.dart
@@ -1,3 +1,7 @@
+import 'dart:math';
+
+import 'package:dartz/dartz.dart';
+
 class Person {
   final int age;
   final String name;
@@ -5,6 +9,7 @@ class Person {
   Person(this.age, this.name);
 } 
 
+// Traditionally 
 void main() {
   final greeting = (String name) {
     return 'Hi $name';
@@ -16,3 +21,29 @@ void main() {
 void saySomething(Function(String) message) {
   print(message('Marcos'));
 }
+
+// With Dartz Package 
+Either<int, String> getPersonData(Person person) {
+  final returnName = Random().nextBool();
+
+  if (returnName) {
+    return Right(person.name);
+  } else {
+    return Left(person.age);
+  }
+}
+
+// or
+// void main() {
+//   final me = Person(21, 'Marcos');
+
+//   final either = getPersonData(me);
+
+//   either.fold(
+//     (age) => print('Your age is $age'),
+//     (name) => print('Your name is $name'),
+//   );
+// }
+
+// Your age is 21
+// Your name is Marcos
